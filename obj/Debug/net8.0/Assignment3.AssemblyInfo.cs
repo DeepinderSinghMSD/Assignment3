c3952abb9b523ed45593e933cd2ae7ff535b1a22
Deepinder Singh <Deepindersingh4359@conestogac.on.ca>
2024-07-20 15:14:40 -0400

    Models created named Product, Cart, Comment, User and Order. There are two more models named CartProduct and OrderProduct for managing many to many relationships between cart, product and order. Moreover, ApplicationDbContext is created for the data.

--- obj/Debug/net8.0/Assignment3.AssemblyInfo.cs
+++ obj/Debug/net8.0/Assignment3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c55019e0e64280566edb0b2725a1f313eeac569")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
